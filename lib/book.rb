0ee120dd120473501f194e16d8f6e0f6db1fe793
Micah Bowie <micahbowie20@gmail.com>
2020-07-07 08:25:32 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,14 +1,9 @@
 class Book 
-  attr_accessor :author, :page_count, :genre
+  attr_accessor :author, :page_count, :genre, :turn_page
+  attr_reader :title
   def initialize(book_name)
     @title = book_name
   end 
-  
-  def title 
-    @title
-  end 
-  
- 
-end 
+ end 
 
 
