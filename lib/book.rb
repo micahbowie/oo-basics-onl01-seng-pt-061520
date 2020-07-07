8428e7b5fdb670ac6313a6df76b1fb9ae64522bc
Micah Bowie <micahbowie20@gmail.com>
2020-07-07 08:26:28 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,13 @@
 class Book 
-  attr_accessor :author, :page_count, :genre, :turn_page
+  attr_accessor :author, :page_count, :genre, 
   attr_reader :title
+ 
   def initialize(book_name)
     @title = book_name
   end 
+  
+  def turn_page
+    puts "Flipping the page wow, you read fast!"
  end 
 
 
